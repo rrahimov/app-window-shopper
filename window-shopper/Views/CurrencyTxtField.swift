c3452eeda452f114940bb8148042444c16141887
Ruhullah Rahimov <rahimov@Ruhullah-MacBook-Pro.local>
2021-01-03 00:02:46 +0400

    custom button

---
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,37 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Ruhullah Rahimov on 02.01.21.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2475794578)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            //string: niye p olsun, placeholder yox?
+            //niye placeholder!, amma p !-siz
+
+        }
+        
+        
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
